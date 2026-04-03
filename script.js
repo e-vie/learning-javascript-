a6b70a3507766f999307cbb20dc5a1f04f56cf07
evie <evelynhan28@gmail.com>
2026-04-03 11:26:03 -0400

    more learning
    more about variables, defining them and reassigning them. currently am reading/learning about strings in javascript

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ let age = seventeen; //equal sign is to assign value, actual equal sign needs to
 console.log(age)
 // age = 17 //testing if the variabl type needs to change //ok so no
 //let age = 17 //testing if i can change the variable type //ok also no
+
+//conclusion: once a variable is defined, you can redefine it without the "let", but need to keep it the same variable type
+//notes: vari. are case sensitive. const can be used like let but it is constant. you can declare a let variable without assigning a value by not typing equal sign or anything after (undefined)
